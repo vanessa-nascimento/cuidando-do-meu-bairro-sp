c71dc936c13f28bc20d0df694f30e02cd7e10938
AndresMRM <andres@inventati.org>
2018-09-30 14:41:29 -0300

    refac

--- src/main.js
+++ src/main.js
@@ -38,7 +38,6 @@ Vue.directive('click-outside', {
   bind: function (el, binding, vnode) {
     el.clickOutsideEvent = function (event) {
       if (!(el === event.target || el.contains(event.target))) {
-        console.log(vnode, binding, event)
         vnode.context[binding.expression](event)
       }
     }
