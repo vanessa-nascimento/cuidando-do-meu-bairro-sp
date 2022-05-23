25d2b4d289bcc92b0968415a4a6ced7aa5f3a1a0
Vanessa Nascimento <vanessa.nascimento@picpay.com>
2022-05-23 00:27:27 -0300

    feat: estilizando form de recurso

--- src/components/perguntas/PerguntaForm.vue
+++ src/components/perguntas/PerguntaForm.vue
@@ -3,14 +3,10 @@
     <div class="modal-body p-6 pb-4 lg:px-8 sm:pb-6 xl:pb-8">
         <h2 class="text-xl font-semibold text-neutral-base">O que deseja perguntar?</h2>
 
-        <p class="text-base text-neutral-base my-4">Aqui você tem como enviar sua dúvida a respeito do gasto. Lembre-se que tanto sua pergunta como a resposta que vai receber do órgão público responsável serão compartilhadas aqui nesse site para todas as pessoas que estiverem interessadas. Para aumentar as chances de receber uma resposta satisfatória, elabore sua pergunta levando em conta algumas dicas, veja <a href="http://cafehacker.prefeitura.sp.gov.br/wp-content/uploads/2014/09/cartilha_acessoainfosp.pdf" target="_blank">aqui</a> algumas delas.
+        <p class="text-base text-neutral-base my-4">Aqui você tem como enviar sua dúvida a respeito do gasto. Lembre-se que tanto sua pergunta como a resposta que vai receber do órgão público responsável serão compartilhadas aqui nesse site para todas as pessoas que estiverem interessadas. Para aumentar as chances de receber uma resposta satisfatória, elabore sua pergunta levando em conta algumas dicas, veja <a class="underline text-secondary-light" href="http://cafehacker.prefeitura.sp.gov.br/wp-content/uploads/2014/09/cartilha_acessoainfosp.pdf" target="_blank">aqui</a> algumas delas.
         </p>
 
-        <p v-if="!bestFitOrgao">
-          <b></b>
-        </p>
-
-        <div id="alert-4" class="flex p-4 mb-4 bg-yellow-100 rounded-lg" role="alert">
+        <div v-if="!bestFitOrgao" id="alert-4" class="flex p-4 mb-4 bg-yellow-100 rounded-lg" role="alert">
           <svg class="flex-shrink-0 w-5 h-5 text-yellow-700" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
           <div class="ml-3 text-sm font-medium text-yellow-700">
             <p>Ops... Parece que não conseguimos achar na lista do E-SIC o órgão responsável por essa despesa. Por favor, escolha um na lista abaixo.</p>
--- src/components/perguntas/RecursoForm.vue
+++ src/components/perguntas/RecursoForm.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="modal-body">
-        <h2>Recorrer</h2>
+    <div class="modal-body modal-body p-6 pb-4 lg:px-8 sm:pb-6 xl:pb-8">
+        <h2 class="text-xl font-semibold text-neutral-base">Recorrer</h2>
 
-        <p>Aqui você tem como enviar seu recurso a respeito do gasto. Lembre-se que tanto sua pergunta como a resposta que vai receber do órgão público responsável serão compartilhadas aqui nesse site para todas as pessoas que estiverem interessadas. Para aumentar as chances de receber uma resposta satisfatória, elabore sua recurso levando em conta algumas dicas, veja <a href="http://cafehacker.prefeitura.sp.gov.br/wp-content/uploads/2014/09/cartilha_acessoainfosp.pdf" target="_blank">aqui</a> algumas delas.
+        <p class="text-base text-neutral-base my-4">Aqui você tem como enviar seu recurso a respeito do gasto. Lembre-se que tanto sua pergunta como a resposta que vai receber do órgão público responsável serão compartilhadas aqui nesse site para todas as pessoas que estiverem interessadas. Para aumentar as chances de receber uma resposta satisfatória, elabore sua recurso levando em conta algumas dicas, veja <a class="underline text-secondary-light" href="http://cafehacker.prefeitura.sp.gov.br/wp-content/uploads/2014/09/cartilha_acessoainfosp.pdf" target="_blank">aqui</a> algumas delas.</p>
 
         <form class="form">
             <div class="form-group">
@@ -10,14 +10,14 @@
                     {{ $t('Question') }}
                 </label>
                 <textarea id="new-recurso-textarea"
-                    class="form-control"
+                    class="form-control bg-white border border-gray-300 text-neutral-base text-sm rounded-lg focus:ring-primary-dark focus:border-primary-dark block w-full p-2.5" 
                     :maxlength="maxPedidoLength"
                     rows="5"/>
                     <p>{{ $t('Remaining characters') }}: {{ remainingChars }}</p>
             </div>
             <button-spinner type="submit"
                     @click.prevent.native="sendRecurso({ key: $route.params.code, orgao, text })"
-                    class="btn btn-color-sec float-right relative">
+                    class="w-full mt-5 text-white cursor-pointer bg-secondary-light hover:bg-secondary-base focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                 {{ $t("Send") }}
             </button-spinner>
         </form>
